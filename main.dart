f44164c341be0210af90e9c8af19586c23e72e7b
PranaavOMG <142111070+PranaavOMG@users.noreply.github.com>
2026-06-01 12:12:13 +0530

    ADD MENU_APP

--- main.dart
+++ main.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-    appBar: AppBar(
-      title: Text("CALCULATOR"), 
-      backgroundColor: const Color.fromARGB(255, 7, 47, 117),
-    ),
-  )),
-  );
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20), color: Colors.blue),
+           
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.all(10),
+                child: Text("HELLO"),
+                  ),
+                Container( 
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10)),
+                  padding: EdgeInsets.all(10),
+                  child: Text("Hello"),
+                  ),
+                ],
+            )),
+            
+          ),
+        ), 
+      );
+  }
 }
