ec3460a76793171d3db93fcf2674d895ae4ae718
PranaavOMG <142111070+PranaavOMG@users.noreply.github.com>
2026-06-01 11:11:32 +0530

    Added the flutter application work

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: Text("CALCULATOR"), 
+      backgroundColor: const Color.fromARGB(255, 7, 47, 117),
+    ),
+  )),
+  );
+}
